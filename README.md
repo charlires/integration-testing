d1e10d5c3957052343271b8460d9dcd0b7041c64
Carlos Andonaegui <candonaegui@ooyala.com>
2016-10-07 17:24:25 -0500

    Add Readme and properties mapper inside tests

--- README.md
+++ README.md
@@ -8,3 +8,8 @@
 - TestNg
 
 ## Getting started
+   
+
+## Util links
+- [Hamcrest article](http://www.vogella.com/tutorials/Hamcrest/article.html)
+- [Unirest java](http://unirest.io/java.html)
--- src/test/java/com/charlires/junittest/DataProviderTest.java
+++ src/test/java/com/charlires/junittest/DataProviderTest.java
@@ -8,8 +8,14 @@ import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.json.JSONException;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
@@ -17,6 +23,8 @@ import static org.junit.Assert.assertThat;
 
 @RunWith(DataProviderRunner.class)
 public class DataProviderTest {
+
+    private static Properties prop = new Properties();
 
     @DataProvider(format = "/users - a: %p[0],  b: %p[1], expected: %p[2]")
     public static Object[][] data() {
@@ -41,6 +49,23 @@ public class DataProviderTest {
         };
     }
 
+    @BeforeClass
+    public static void init()  {
+        try {
+
+            InputStream input = FileInputStream.class.getResourceAsStream("/env.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("host"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
     @Test
     @UseDataProvider("data")
     public void testAddNumbers(int a, int b, int expected) {
@@ -50,7 +75,7 @@ public class DataProviderTest {
     @Test
     @UseDataProvider("dataRest")
     public void testGet(String name, String expected) throws JSONException, UnirestException {
-        HttpResponse<JsonNode> response = Unirest.get("http://httpbin.org/get")
+        HttpResponse<JsonNode> response = Unirest.get(prop.getProperty("host"))
                 .queryString("name", name).asJson();
         assertEquals(response.getBody().getObject().getJSONObject("args").getString("name"), expected);
     }
@@ -63,4 +88,3 @@ public class DataProviderTest {
     }
 
 }
-
